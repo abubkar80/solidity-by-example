f834eefb8b7fb848f64ed23962f524f42d89793c
abubkar80 <abubakarusmanmuhammad80@gmail.com>
2025-05-13 23:43:41 +0100

    Solidity-By-Example

---
+++ Contracts/constants.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+contract Constants {
+    // set a constant address
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+
+    // set a constant variable
+    uint256 public constant MY_INTEGER = 159;
+}
