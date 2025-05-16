e797676afa706a254a993cbe41b12fec9c02f8d8
abubkar80 <abubakarusmanmuhammad80@gmail.com>
2025-05-16 03:15:16 +0100

    added some comment to the code for better understanding

--- Contracts/votingSystem.sol
+++ Contracts/votingSystem.sol
@@ -11,36 +11,49 @@ contract votingSystem{
     // set the voting deadline using immutable
     uint256 public immutable votingDeadline;
 
+    // create variable to store is voting is open or not
     bool public isClosed;
 
+    // create a varialble to track voters count
     uint256 public voteCount;
 
+    //check if person has voted
     mapping(address => bool) public  hasVoted;
 
+    // constructor to set the deadline
     constructor(){
         votingDeadline = block.timestamp + 1 days;
     }
 
-     function closeVoting() public {
+    // function to check if the voting is closed
+    function closeVoting() public {
         require(!isClosed, "it is already close");
-    
         isClosed = true;
     }
 
-    // check if the vote is open 
+    // check if the vote is open and close when close
     function   isVotingOpen() public view returns (bool){
         return block.timestamp < votingDeadline && !isClosed ;
     }
 
     // function to cast vote
     function vote() public {
+        // check if the voting is open 
         require(isVotingOpen(),"Has voted");
+
+        // check if the deployer already voted
         require(!hasVoted[msg.sender],"Already Voted");
+
+        // making sure the MAX_VOTERS is not exceeded
         require(voteCount< MAX_VOTERS, "Max voters reached");
+
+        // checking if the vote is close or not
         require(!isClosed, "it has been close");
 
+        // changing the state of the sender when voted
         hasVoted[msg.sender] = true;
 
+        // increaing the voting count
         voteCount++;
     }
 
